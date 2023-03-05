af8f084bf62ddd8d72274f32ab1e2d1a0a1f400c
jigarpparekh <jigarpparekh26@yahoo.com>
2023-03-05 17:41:27 +0530

    Initial Commit
    Blank structure for API and Angular Project

---
+++ NgPremiumCalculator/src/app/calculator/calculator-customerdetail/calculator-customerdetail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-customerdetail',
+  template: '<h1>Customer Details</h1>'
+})
+
+export class CalculatorCustomerdetailComponent{
+
+}
