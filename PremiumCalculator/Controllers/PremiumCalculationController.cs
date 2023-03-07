4eafe405afbe296637cc6b731c08c62898482288
jigarpparekh <jigarpparekh26@yahoo.com>
2023-03-07 17:47:13 +0530

    Added API Changes for Premium calulation

---
+++ PremiumCalculator/Controllers/PremiumCalculationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PremiumCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PremiumCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremiumCalculationController : ControllerBase
+    {
+        // GET: api/<PremiumCalculationController>
+        [HttpGet("{SumInsured}/{OccupationRating}/{Age}")]
+        public IEnumerable<string> Get(int SumInsured, double OccupationRating, double Age)
+        {
+            if (Age <= 70)
+            {
+                Premium premium = new();
+
+                premium.DeathPremium = (float)((SumInsured * OccupationRating * Age) / 1000 * 12);
+                premium.TPDPremiumMonthly = (float)((SumInsured * OccupationRating * Age) / 1234);
+
+                string json = JsonConvert.SerializeObject(premium);
+                return new string[] { premium.DeathPremium.ToString("0.00"), premium.TPDPremiumMonthly.ToString("0.00") };
+            }
+
+            else
+            {
+                //return new HttpResponseMessage(HttpStatusCode.NotAcceptable);
+                return new string[] { HttpStatusCode.NotAcceptable.ToString(), "Age is out of limit" };
+            }
+        }
+
+
+    }
+}
---
+++ PremiumCalculator/Models/Premium.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PremiumCalculator.Models
+{
+    public class Premium
+    {
+        public int SumInsured { get; set; }
+        public double OccupationRating { get; set; }
+        public double Age { get; set; }
+
+        public float DeathPremium { get; set; }
+        public float TPDPremiumMonthly { get; set; }
+    }
+}
